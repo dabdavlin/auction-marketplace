aaae0782787c2221b9af4ab7ed48800666c0b27a
dabdavlin <georgetrevord@gmail.com>
2022-11-23 10:49:48 -0600

    added listItem

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,6 @@ const Home = () => {
   const { data: listings, isLoading: loadingListings } =
     useActiveListings(contract);
 
-  console.log(listings);
-
   return (
     <div className="">
       <Header />
